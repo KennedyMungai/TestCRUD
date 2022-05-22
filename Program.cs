9c7e0d7c66898f8196d6893a3be3a769a53de015
KennedyMungai <kennedymungaifmab@gmail.com>
2022-05-22 14:48:26 +0300

    Added the DbContext class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using System.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using TestCRUD.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
 
